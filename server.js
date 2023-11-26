54183fa23ccab2826cdd38ec5848e67687101e9a
Adonis3k <adonisdmadera@gmail.com>
2023-11-26 13:38:51 -0500

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : 'process.env.postgresql-cubic-03454',
+    connectionString : 'process.env.DATABASE_URL',
     ssl: true,
   }
 });
