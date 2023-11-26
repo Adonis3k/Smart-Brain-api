a73656cd09a60c313046398eac43957943ae40e9
Adonis3k <adonisdmadera@gmail.com>
2023-11-26 11:47:50 -0500

    adding port

--- server.js
+++ server.js
@@ -33,9 +33,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
-
